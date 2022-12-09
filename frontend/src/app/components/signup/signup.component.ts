9fad2a9f15dad38af01c37e507d652557cd09b6c
Anton Revta <anton.revta@gmail.com>
2022-12-09 17:59:36 +0300

    init frontend part, add signup form

---
+++ frontend/src/app/components/signup/signup.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent implements OnInit {
+  form: FormGroup;
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  signup() {
+    console.log(this.form.value);
+  }
+}
